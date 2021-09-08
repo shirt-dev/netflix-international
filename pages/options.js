099dfd01075912838991aa7594701a140553d429
shirtjs <2660574+shirt-dev@users.noreply.github.com>
2021-09-08 10:51:45 -0400

    Fix indentation, bump version

--- pages/options.js
+++ pages/options.js
@@ -1,36 +1,37 @@
+/* eslint-disable no-undef */
 function save_options() {
-    const use6Channels = document.getElementById("use51").checked;
-    const showAllTracks = document.getElementById("showAllTracks").checked;
-    const setMaxBitrate = document.getElementById("setMaxBitrate").checked;
-    const disableVP9 = document.getElementById("disableVP9").checked;
+	const use6Channels = document.getElementById("use51").checked;
+	const showAllTracks = document.getElementById("showAllTracks").checked;
+	const setMaxBitrate = document.getElementById("setMaxBitrate").checked;
+	const disableVP9 = document.getElementById("disableVP9").checked;
 
-    chrome.storage.sync.set({
-        use6Channels,
-        showAllTracks,
-        setMaxBitrate,
-        disableVP9,
-    }, function() {
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
-    });
+	chrome.storage.sync.set({
+		use6Channels,
+		showAllTracks,
+		setMaxBitrate,
+		disableVP9,
+	}, function() {
+		var status = document.getElementById("status");
+		status.textContent = "Options saved.";
+		setTimeout(function() {
+			status.textContent = "";
+		}, 750);
+	});
 }
 
 function restore_options() {
-    chrome.storage.sync.get({
-        use6Channels: true,
-        showAllTracks: true,
-        setMaxBitrate: false,
-        disableVP9: false,
-    }, function(items) {
-        document.getElementById("use51").checked = items.use6Channels;
-        document.getElementById("showAllTracks").checked = items.showAllTracks;
-        document.getElementById("setMaxBitrate").checked = items.setMaxBitrate;
-        document.getElementById("disableVP9").checked = items.disableVP9;
-    });
+	chrome.storage.sync.get({
+		use6Channels: true,
+		showAllTracks: true,
+		setMaxBitrate: false,
+		disableVP9: false,
+	}, function(items) {
+		document.getElementById("use51").checked = items.use6Channels;
+		document.getElementById("showAllTracks").checked = items.showAllTracks;
+		document.getElementById("setMaxBitrate").checked = items.setMaxBitrate;
+		document.getElementById("disableVP9").checked = items.disableVP9;
+	});
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", save_options);
